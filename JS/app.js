c4292f3269061a62785a9c48d0864dddbc34a876
92radar <viepro.db@gmail.com>
2026-04-08 11:14:44 +0200

    feat: Update Datadog service name to match application identifier

--- JS/app.js
+++ JS/app.js
@@ -4,7 +4,7 @@ window.DD_RUM && window.DD_RUM.init({
     applicationId: '9e03d4d1-9c3e-4b3f-9090-0b19766e2bf5',
     clientToken: 'pub573dbd4f632db05201c5bbee64843458',
     site: 'datadoghq.eu',
-    service: 'ECORIDE',
+    service: 'eco-ride.online',
     env: 'prod',
     version: '1.0.0',
     sessionSampleRate: 100,
